9a4aba37e184791cc349c161b43cec4e284fc787
brainy <devbrainy98@gmail.com>
2022-07-18 06:37:09 +0100

    committed task files

--- links/models.py
+++ links/models.py
@@ -2,6 +2,7 @@ from django.utils import timezone
 from django.db import models
 from django.contrib.auth import get_user_model
 from .utils import generate_random_id
+from .managers import ActiveLinkManager
 
 # Create your models here.
 class Link(models.Model):
@@ -11,6 +12,8 @@ class Link(models.Model):
     author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     created_date = models.DateTimeField(default=timezone.now)
     active = models.BooleanField(default=True)
+    objects = models.Manager(),
+    public = ActiveLinkManager(),
 
     def save(self, *args, **kwargs):
         self.identifier = generate_random_id()
